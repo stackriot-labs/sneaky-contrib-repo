c33e0dd2d8feb18667f38197840eafe80baa3793
Seth Bergman <seth.atxwebs@gmail.com>
2018-09-03 05:05:02 -0500

    updated contributions

--- generate_contribs.js
+++ generate_contribs.js
@@ -6,8 +6,8 @@ const { exec } = require('child_process')
 const execAsync = promisify(exec)
 const writeFileAsync = promisify(writeFile)
 
-const AUTHOR = 'username'
-const EMAIL = 'email'
+const AUTHOR = 'sethbergman'
+const EMAIL = 'seth.atxwebs@gmail.com'
 const DAYS_TO_GENERATE = 365
 
 const dateMinusGivenDays = days => {
